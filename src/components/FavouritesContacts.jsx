5f50a450fa95f1e17add3ae56f004f6ebba4a112
NirjharJoshi <njupkiet@gmail.com>
2023-03-30 16:48:43 +0530

    ui extra spacing in right for some screens resolved

--- src/components/FavouritesContacts.jsx
+++ src/components/FavouritesContacts.jsx
@@ -3,9 +3,9 @@ import Contact from "./ui/Contact";
 
 const FavouritesContacts = () => {
   return (
-    <div className="my-2 flex flex-col items-center gap-y-4">
+    <div className="my-2 py-2 px-4 flex flex-col items-center gap-y-4">
       <h3 className="font-bold text-neutral-300">Favourites</h3>
-      <div className="lg:flex lg:flex-row lg:flex-wrap lg:gap-4 lg:px-28">
+      <div className="lg:flex lg:flex-row lg:flex-wrap lg:gap-4 lg:justify-center lg:flex-grow">
         <Contact />
         <Contact />
         <Contact />
--- src/components/GeneralContacts.jsx
+++ src/components/GeneralContacts.jsx
@@ -5,7 +5,7 @@ const GeneralContacts = () => {
   return (
     <div className="my-2 flex flex-col items-center gap-y-4">
       <h3 className="font-bold text-neutral-300">General Contacts</h3>
-      <div className="lg:flex lg:flex-row lg:flex-wrap lg:gap-4 lg:px-28">
+      <div className="lg:flex lg:flex-row lg:flex-wrap lg:gap-4 lg:justify-center lg:flex-grow">
         <Contact />
         <Contact />
         <Contact />
